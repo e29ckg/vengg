2bf316427a6c0f41103365fa3ee66a6598f712e5
e29ckg <e29ckg@gmail.com>
2023-06-18 23:59:50 +0700

    18-06-66๘2

--- server/users/user.php
+++ server/users/user.php
@@ -31,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             echo json_encode($response);
             exit;
         }
-
+        
         $response = [
             'status' => false,
             'message' => 'No data found'
